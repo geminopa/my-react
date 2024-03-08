a7fc126ad99a1a664c35c48e63a8f4b60ec03dfd
geminopa <genshi32@icloud.com>
2024-03-08 21:48:43 +0900

    props,useStateの使い方基礎

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import EventBasic from './chap03/EventBasic';
+import StateBasic from './chap03/StateBasic';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <EventBasic type="time" />
+    <StateBasic init={0} />
   </React.StrictMode>
 );
 
